90475c4a814e092eb53fdb0d0b7b6927d39cdc35
yraygoza <yanelliraygoza@gmail.com>
2025-05-11 23:45:29 -0700

    Added prework to repository

---
+++ IOS code path/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  IOS code path
+//
+//  Created by Yanelli Raygoza on 5/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+       
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    
+    func changeColor() -> UIColor{
+        
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+    }
+}
+
